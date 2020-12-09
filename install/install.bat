415d671063eba2bd402c0b499d94bcf8515b6e43
Dmitrii Olkhov <dmitrii.olkhov@NetCracker.com>
2020-12-10 00:44:50 +0400

    Fixed installation script and container user

--- install/install.bat
+++ install/install.bat
@@ -1,3 +1,7 @@
 @echo off
 set namespace=%1
+kubectl create namespace %namespace%
+kubectl create serviceaccount operator --namespace %namespace%
+kubectl create clusterrole deployment-operator --verb=get,watch,list,create,update,patch --resource=deployments.apps
+kubectl create clusterrolebinding deployment-operator --clusterrole=deployment-operator --serviceaccount=%namespace%:operator
 kubectl apply -f .\operator\deployment.yaml -n %namespace%
