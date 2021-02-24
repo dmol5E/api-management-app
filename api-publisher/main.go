a9ce10231d46aa5edf64603a9c6ad686c5af45a1
Dmitrii Olkhov <reminder63@gmail.com>
2021-02-24 22:35:08 +0400

    Fixed api-publisher

--- api-publisher/main.go
+++ api-publisher/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
 func main() {
-	println("Hello!")
+
+	log.SetOutput(os.Stdout)
+	log.Println("Starting application")
+
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("/health")
+		fmt.Fprint(w, "{\"status\":\"UP\"}")
+	})
+
+	log.Println("App has started on port 0.0.0.0:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
